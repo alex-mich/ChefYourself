94db5a3c99f45aa5cda11d05190247cc48bd6ba8
korovts <korovts@gmail.com>
2016-06-16 01:21:49 +0300

    - Functionality @ Ingredients Response (ADDED)

--- src/main/java/controllers/ResponseController.java
+++ src/main/java/controllers/ResponseController.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,8 +11,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.gson.Gson;
+
+import pojos.Language;
 import pojos.RequestObject;
 import pojos.ResponseObject;
+import pojos.TranslatedIngredient;
 import services.ResponseService;
 
 @Controller
@@ -25,5 +31,16 @@ public class ResponseController {
 		 ResponseObject responseObject = responseService.recipesResponse(requestObject1);
 		 return responseObject;
 	 }
-		 
+		
+	 @RequestMapping(value="/ingredients.ds", method = RequestMethod.POST)
+	 public @ResponseBody ArrayList<String> ingredientsResponse(@RequestBody String locale) {
+		 Gson gson = new Gson();
+		 Language language = gson.fromJson(locale, Language.class);
+		 List<TranslatedIngredient> translatedIngredientList =  responseService.ingredientsResponse(language);
+		 ArrayList<String> ingredientNameList = new ArrayList<String>();
+		 for(TranslatedIngredient translatedIngredient: translatedIngredientList){
+			 ingredientNameList.add(translatedIngredient.getIname());
+		 }
+		 return ingredientNameList;
+	 }
 }
